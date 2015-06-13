f3010f2e109fd72cd61409ccc40a54af46abf561
Louie Dinh <LouieDinh@gmail.com>
2015-06-13 15:18:16 -0700

    Add sensitivity to sample sizes in simulation

--- mle.r
+++ mle.r
@@ -1,17 +1,21 @@
 # Let the underlying population proportion vary and just look at the distribution.
 
-samples <- function(theta, n_samples) {
+samples <- function(theta, sample_sizes) {
   # Assume each sample is of size 50
-  rbinom(n_samples, 50, theta)
+  res <- c()
+  for(idx in seq_along(sample_sizes)) {
+    res[idx] <- rbinom(n=1, size=sample_sizes[idx], theta)
+  }
+  
+  res
 }
 
-run_simulation <- function(n) {
-  n_samples = 2700
-  theta = .002
+run_simulation <- function(n, trap_counts) {
+  theta = .006
   
   res = NULL
   for(i in 1:n) {
-    experiment <- samples(theta=theta, n_samples=n_samples)
+    experiment <- samples(theta=theta, sample_sizes=trap_counts)
     df <- data.frame(neg=sum(experiment == 0), pos=sum(experiment > 0))
     res = rbind(res, df)
   }
@@ -27,5 +31,5 @@ print("Actual:")
 print(presence)
 
 print("Simulated:")
-print(run_simulation(10))
+print(run_simulation(10, species_data$NumMosquitos))
 
