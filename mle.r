da4ff3d68288e5c5d0421cc84ce230325d372fa6
Louie Dinh <LouieDinh@gmail.com>
2015-06-13 16:36:46 -0700

    Implementation of brute force on the likelihood function

--- mle.r
+++ mle.r
@@ -10,9 +10,7 @@ samples <- function(theta, sample_sizes) {
   res
 }
 
-run_simulation <- function(n, trap_counts) {
-  theta = .006
-  
+run_simulation <- function(n, trap_counts, theta) {
   res = NULL
   for(i in 1:n) {
     experiment <- samples(theta=theta, sample_sizes=trap_counts)
@@ -23,13 +21,61 @@ run_simulation <- function(n, trap_counts) {
   res
 }
 
-# Looks like it should be ~.002
-species_data <- filter(trai, Species=="CULEX PIPIENS")
-presence <- table(temp$WnvPresent)
-names(presence) <- c("neg", "pos")
-print("Actual:")
-print(presence)
+run_simple <- function(theta) {
+  # Looks like it should be ~.002
+  species_data <- filter(trai, Species=="CULEX PIPIENS")
+  presence <- table(temp$WnvPresent)
+  names(presence) <- c("neg", "pos")
+  print("Actual:")
+  print(presence)
+  
+  print("Simulated:")
+  print(run_simulation(10, species_data$NumMosquitos, theta))
+}
 
-print("Simulated:")
-print(run_simulation(10, species_data$NumMosquitos))
+##### Try 2 - likelihood function
 
+likelihood <- function(data, theta) {
+  # Calculates the P(data | theta)
+  # = product of individual probabilities
+  # The probability that a specific detection of wnv is
+  # 1 - prob(no detection) and prob(no virus) = (1 - infection_rate)^n
+  p_no_virus = (1 - theta)
+  
+  counts <- data$NumMosquitos
+  wnv <- data$WnvPresent
+  
+  log_likelihood = 0
+  p_detections = c()
+  p_no_detections = c()
+  for(idx in seq_along(counts)){
+    count <- counts[idx]
+    p_no_detection = p_no_virus ^ count
+    p_detection = 1 - p_no_detection
+    
+    p_detections[idx] = p_detection
+    p_no_detections[idx] = p_no_detection
+  }
+  
+  df <- data.frame(neg=log(p_no_detections), pos=log(p_detections), data$WnvPresent, data$NumMosquitos)
+  
+  # Add the likelihood of presence on the detected with the absence in the non-detected
+  likelihood = sum(df$pos[data$WnvPresent == 1]) + sum(df$neg[data$WnvPresent == 0])
+        
+  likelihood
+}
+
+run_likelihood <- function() {
+  species_data <- filter(trai, Species=="CULEX PIPIENS")
+  infection_rates = seq(.005, .007, by=.0001)
+  likelihoods <- sapply(infection_rates, likelihood, data=species_data)
+  data.frame(p=infection_rates, likelihood=likelihoods)
+}
+
+
+# Wow! They agree!
+print("Using naive simulation with theta=.006")
+run_simple(.006)
+
+print("Using likelihood")
+print(run_likelihood())
